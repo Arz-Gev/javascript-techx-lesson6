d37b100380433e799324ce83254a1c8df5395042
Gevorg Arzakanyan <zokavski@gmail.com>
2025-07-27 13:40:34 +0400

    task 7 is done

---
+++ task-7/script.js
@@ -0,0 +1,34 @@
+let start = document.getElementById("start");
+let end = document.getElementById("end");
+let display = document.getElementById("display");
+let ticking = false;
+let tick = false;
+let tickTack;
+
+start.addEventListener("click", () => Ticker(true));
+
+end.addEventListener("click", () => Ticker(false));
+
+document.addEventListener("keydown", () => {
+  ticking ? Ticker(false) : Ticker(true);
+});
+
+function Ticker(shouldWork) {
+  if (shouldWork) {
+    ticking = true;
+    tickTack = setInterval(() => {
+      console.log(tick);
+      if (tick) {
+        display.textContent = "TACK";
+        tick = false;
+        return;
+      }
+      display.textContent = "TICK";
+      tick = true;
+    }, 1000);
+  } else {
+    ticking = false;
+    clearInterval(tickTack);
+    display.textContent = "PRESS TO START";
+  }
+}
